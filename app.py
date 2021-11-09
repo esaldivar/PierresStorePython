1cf5175eff8ecdd695bd76690c046a66fa955f6d
Eric Saldivar <eric.saldivar@mindbodyonline.com>
2021-11-09 15:04:19 -0600

    getting ready to tie react and flask together

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+@app.route('/<string:name>')
+def greet(name):
+    return f'Hello {name}'
 
 @app.route('/')
 def hello_world():
-    return 'Hello World.'
+    request_method = request.method
+    return render_template('index.html', request_method=request_method)
 
 if __name__ == '__main__':
     app.run(debug=True)
