b960bb3a57257557ed353dc91b606c4a7af4aa19
Eric Saldivar <eric.saldivar@mindbodyonline.com>
2021-11-10 10:22:26 -0600

    updated app to typescript file type

--- client/App.tsx
+++ client/App.tsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 
 export const App= () => {
   
-    let apple: string;
-    apple = 9;
 
     return (
     <div>
-        <h1 className="text-green-600">Hello from Eric {apple}</h1>
+        <h1 className="text-green-600">Hello from Eric</h1>
 
     </div>
   )
