903114f594920b6fbe20720c8bffb94e7e7511af
Eric Saldivar <eric.saldivar@mindbodyonline.com>
2021-11-10 10:06:26 -0600

    configured tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ export const App= () => {
 
     return (
     <div>
-        <h1 className="hello">Hello from Eric</h1>
+        <h1 className="text-green-600">Hello from Eric</h1>
 
     </div>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./server/templates/*.html', './client/src/**/*.js',],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
