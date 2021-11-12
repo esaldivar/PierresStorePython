9f123c7e431f3e9adb0391da0f38b1fb953e1353
Eric Saldivar <eric.saldivar@mindbodyonline.com>
2021-11-12 10:53:25 -0600

    successfully initiated graphql with flask

---
+++ server/main.py
@@ -0,0 +1,49 @@
+from flask import Flask, send_from_directory
+from api import app, db
+from api import models
+from ariadne import load_schema_from_path, make_executable_schema, \
+    graphql_sync, snake_case_fallback_resolvers, ObjectType
+from ariadne.constants import PLAYGROUND_HTML
+from flask import request, jsonify
+from api.queries import resolve_todos
+
+query = ObjectType("Query")
+
+query.set_field("todos", resolve_todos)
+
+type_defs = load_schema_from_path("schema.graphql")
+schema = make_executable_schema(
+    type_defs, query, snake_case_fallback_resolvers
+)
+
+
+@app.route("/graphql", methods=["GET"])
+def graphql_playground():
+    return PLAYGROUND_HTML, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    data = request.get_json()
+
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        debug=app.debug
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+
+@app.route("/login")
+def login():
+    return 'hello from the flask server'
+
+if __name__ == '__main__':
+    app.run(debug=True)
