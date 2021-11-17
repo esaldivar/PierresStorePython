11a66bdcd590bff34ea34ef5791cb508a34fc0b7
Eric Saldivar <eric.saldivar@mindbodyonline.com>
2021-11-17 09:29:49 -0600

    testing husky for git add

--- client/App.tsx
+++ client/App.tsx
@@ -8,13 +8,13 @@ import { ReduxTester } from './views/reduxTest';
 export const App = () => {
   const [getMessage, setGetMessage] = useState<any>('');
 
-  const apple = "adjfkasfjlasdf"
-  
+  const apple = 'adjfkasfjlasdf';
+
   useEffect((): void => {
     axios
       .get('/login')
       .then((response) => {
-        console.log("SUCCESS", response);
+        console.log('SUCCESS', response);
         setGetMessage(response.data);
       })
       .catch((error) => {
@@ -27,8 +27,8 @@ export const App = () => {
       <Provider store={store}>
         <h1 className="text-blue-600">Hello from Auroraa</h1>
         <p className="text-red-600">{getMessage}</p>
-                    <ReduxTester />
-                <Login />
+        <ReduxTester />
+        <Login />
       </Provider>
     </div>
   );
