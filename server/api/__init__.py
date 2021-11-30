7d458f9ee8e316a306a65c6463f303fe68101e0a
Eric Saldivar <Eric.Saldivar@mindbodyonline.com>
2021-11-30 10:50:49 -0600

    no longer using literal URI

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 
 app = Flask(__name__, static_url_path='', static_folder='../../dist')
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://kpyzmoqk:LCpqqoTqkCXkoit6ESNhynEyJq24ewol@castor.db.elephantsql.com/kpyzmoqk"
+app.config["SQLALCHEMY_DATABASE_URI"] = f'{os.getenv("POSTGRESQL_URI")}'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
