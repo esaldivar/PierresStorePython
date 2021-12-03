cb17fddc85d5e9329eae0378e5de609a3c24a9f3
Eric Saldivar <Eric.Saldivar@mindbodyonline.com>
2021-12-03 14:40:55 -0600

    able to search for a singular product using axios request to graphql in the search bar

--- client/views/SearchResult.tsx
+++ client/views/SearchResult.tsx
@@ -1,13 +1,10 @@
 import { RootStateOrAny, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const SearchResult = () => {
   const { searchViewPage } = useSelector(
     (state: RootStateOrAny) => state.inventory
   );
-
-  const [example, setExample] = useState<any>('pears');
 
   return <div>{searchViewPage.length > 0 && <h1>{searchViewPage}</h1>}</div>;
 };
