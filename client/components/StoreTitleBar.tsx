2b90a4131b61523c26a761f2e55bcd3174e889d5
Eric Saldivar <Eric.Saldivar@mindbodyonline.com>
2021-12-08 09:49:33 -0600

    created redux types and converting functions to helper functions in util folder

--- client/components/StoreTitleBar.tsx
+++ client/components/StoreTitleBar.tsx
@@ -1,11 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
-
 import { useAppSelector, useAppDispatch, RootState } from '../types/reduxTypes';
 import { bindActionCreators } from 'redux';
 import { inventoryActionCreator } from '../redux/actionReferences';
-import { singleResult } from '../types/storeTypes';
-import { alphabetize } from '../utilities/helperFuncs';
+import {
+  alphabetize,
+  reverseAlpha,
+  lowPrice,
+  highPrice,
+} from '../utilities/helperFuncs';
 
 const StoreTitleBar = () => {
   const { store } = useAppSelector((state: RootState) => state.inventory);
@@ -14,36 +17,6 @@ const StoreTitleBar = () => {
     inventoryActionCreator,
     dispatch
   );
-
-  const reverseAlpha = () => {
-    sortInventory(
-      [...store].sort((a: singleResult, b: singleResult) => {
-        const nameA = a.productName.toUpperCase();
-        const nameB = b.productName.toUpperCase();
-        return nameA > nameB ? -1 : nameA < nameB ? 1 : 0;
-      })
-    );
-  };
-
-  const lowPrice = () => {
-    sortInventory(
-      [...store].sort((a: singleResult, b: singleResult) => {
-        const priceA = parseInt(a.price);
-        const priceB = parseInt(b.price);
-        return priceA < priceB ? -1 : priceA > priceB ? 1 : 0;
-      })
-    );
-  };
-
-  const highPrice = () => {
-    sortInventory(
-      [...store].sort((a: singleResult, b: singleResult) => {
-        const priceA = parseInt(a.price);
-        const priceB = parseInt(b.price);
-        return priceA > priceB ? -1 : priceA < priceB ? 1 : 0;
-      })
-    );
-  };
 
   return (
     <div className="storeTitle">
@@ -65,7 +38,11 @@ const StoreTitleBar = () => {
             </button>
           </div>
           <div>
-            <button onClick={reverseAlpha}>
+            <button
+              onClick={() => {
+                reverseAlpha(sortInventory, store);
+              }}
+            >
               <FontAwesomeIcon
                 className="searchIcon"
                 icon={faCaretDown}
@@ -85,7 +62,11 @@ const StoreTitleBar = () => {
         <h3>Price</h3>
         <div>
           <div>
-            <button onClick={lowPrice}>
+            <button
+              onClick={() => {
+                lowPrice(sortInventory, store);
+              }}
+            >
               <FontAwesomeIcon
                 className="searchIcon"
                 icon={faCaretUp}
@@ -94,7 +75,11 @@ const StoreTitleBar = () => {
             </button>
           </div>
           <div>
-            <button onClick={highPrice}>
+            <button
+              onClick={() => {
+                highPrice(sortInventory, store);
+              }}
+            >
               <FontAwesomeIcon
                 className="searchIcon"
                 icon={faCaretDown}
--- client/utilities/helperFuncs.ts
+++ client/utilities/helperFuncs.ts
@@ -1,11 +1,41 @@
 import { Stores, singleResult } from '../types/storeTypes';
 
-export const alphabetize = (action: any, state: Stores) => {
+export const alphabetize = (action: any, state: Stores): void => {
   action(
     [...state].sort((a: singleResult, b: singleResult) => {
       const nameA = a.productName.toUpperCase();
       const nameB = b.productName.toUpperCase();
       return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+    })
+  );
+};
+
+export const reverseAlpha = (action: any, state: Stores): void => {
+  action(
+    [...state].sort((a: singleResult, b: singleResult) => {
+      const nameA = a.productName.toUpperCase();
+      const nameB = b.productName.toUpperCase();
+      return nameA > nameB ? -1 : nameA < nameB ? 1 : 0;
+    })
+  );
+};
+
+export const lowPrice = (action: any, state: Stores): void => {
+  action(
+    [...state].sort((a: singleResult, b: singleResult) => {
+      const priceA = parseInt(a.price);
+      const priceB = parseInt(b.price);
+      return priceA < priceB ? -1 : priceA > priceB ? 1 : 0;
+    })
+  );
+};
+
+export const highPrice = (action: any, state: Stores): void => {
+  action(
+    [...state].sort((a: singleResult, b: singleResult) => {
+      const priceA = parseInt(a.price);
+      const priceB = parseInt(b.price);
+      return priceA > priceB ? -1 : priceA < priceB ? 1 : 0;
     })
   );
 };
