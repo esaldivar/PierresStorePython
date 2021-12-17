9ea8c47d62810211858df611cd67933e81843bc7
Eric Saldivar <Eric.Saldivar@mindbodyonline.com>
2021-12-17 15:18:44 -0600

    categories work besides favorites

--- client/views/Categories.tsx
+++ client/views/Categories.tsx
@@ -2,7 +2,7 @@ import SpecificCategory from '../components/SpecificCategory';
 
 const Categories = () => {
   return (
-    <div>
+    <div className="w-3/4 px-2 m-auto">
       <SpecificCategory />
     </div>
   );
