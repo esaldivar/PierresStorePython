1475fca1087c9c48f51b2a3793209a4d27729dfa
Eric Saldivar <Eric.Saldivar@mindbodyonline.com>
2021-12-17 14:11:37 -0600

    working on user nav portion

--- client/components/UserNav.tsx
+++ client/components/UserNav.tsx
@@ -1,22 +1,34 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const UserNav = () => {
   return (
-    <div className="flex-col items-center mr-6 align-middle cursor-pointer">
-      <div className="flex items-center align-middle">
-        <FontAwesomeIcon
-          className="fill-current text-primaryBrown "
-          icon={faUserCircle}
-          size="3x"
-        />
-        <FontAwesomeIcon
-          className="ml-2 fill-current text-primaryBrown "
-          icon={faChevronDown}
-          size="1x"
-        />
-      </div>
-      <h1 className="mt-2 font-bold text-center text-primaryBrown">Sign in</h1>
+    <div className="flex items-center mr-4 align-middle cursor-pointer">
+      <Dropdown className="flex ">
+        <Dropdown.Toggle
+          variant="success"
+          id="dropdown-basic"
+          className="flex items-center text-3xl align-middle bg-transparent border-none text-primaryBrown"
+        >
+          <div className="flex-col">
+            <FontAwesomeIcon
+              className="fill-current text-primaryBrown "
+              icon={faUserCircle}
+              size="1x"
+            />
+            <h1 className="text-sm font-bold text-center text-primaryBrown">
+              Sign in
+            </h1>
+          </div>
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu className="py-0 overflow-y-auto bg-lightBrown text-greenTitle">
+          <Dropdown.Item className="py-0 m-auto text-greenTitle hover:text-white hover:bg-greenTitle">
+            LAFDsadfasdf
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 };
