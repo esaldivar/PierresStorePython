011a8671d69db11e97fa8c73834c7f4f91af5e18
Eric Saldivar <Eric.Saldivar@mindbodyonline.com>
2021-12-20 12:49:47 -0600

    preparing for user auth tables and favorites

--- client/views/SignIn.tsx
+++ client/views/SignIn.tsx
@@ -7,6 +7,20 @@ const SignIn = () => {
         Sign into your account.
       </h1>
       <SignInForm />
+      <button
+        type="submit"
+        value="Forgot password?"
+        className="w-full py-2 mt-6 underline text-primaryBrown "
+      >
+        Forgot password?
+      </button>
+      <button
+        type="submit"
+        value="Create account"
+        className="w-full py-2 mt-6 bg-white bg-opacity-50 border-2 rounded text-greenTitle border-greenTitle "
+      >
+        Create your account
+      </button>
     </div>
   );
 };
